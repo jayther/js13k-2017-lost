c87647b4786bd9efc3e34d4fc7aeb31b8ae8df8e
jayther <jayther@gmail.com>
2017-08-14 05:14:01 -0700

    HHHAAAAAANNNNDDDSSS

---
+++ src/js/src-es6/main.js
@@ -0,0 +1,11 @@
+class Main {
+  constructor() {
+    this.root = document.getElementById('root');
+    this.test = '';
+  }
+  init() {
+    this.test = 'rawr';
+  }
+}
+
+window.game = new Main();

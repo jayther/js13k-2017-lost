9d9a574befdf2ae2eab9d4073dd5798e027dd859
jayther <jayther@gmail.com>
2017-09-13 02:19:08 -0700

    Generatorrrrrrrrrrr stuff

--- src/js/src/play/World.js
+++ src/js/src/play/World.js
@@ -2,25 +2,250 @@
 function World() {
   DisplayContainer.apply(this, arguments);
   this.grid = {};
-  this.cellSize = 50;
+  this.cellSize = 20;
+  this.gridRange = {
+    minWidth: 75,
+    maxWidth: 100,
+    minHeight: 75,
+    maxHeight: 100
+  };
+  this.hallwayMinChunkArea = 1000;
+  this.hallwayMinChunkWidth = 20;
+  this.hallwayMinChunkHeight = 20;
+  this.minChunkArea = 100;
+  this.minChunkWidth = 10;
+  this.minChunkHeight = 10;
+  this.hallwaySize = 2;
+  this.gridWidth = 0;
+  this.gridHeight = 0;
 }
+World.cellTypes = {
+  ground: 1,
+  outerWall: 2,
+  door: 3,
+  roomGround: 4
+};
 
 World.prototype = extendPrototype(DisplayContainer.prototype, {
   generate: function () {
-    for (var i = 0; i < 20; i += 1) {
-      var x = Math.floor(Math.random() * 10);
-      var y = Math.floor(Math.random() * 10);
-      this.setPos(x, y, Math.random() < 0.5 ? 0 : 1);
+    var w = Random.rangeInt(this.gridRange.minWidth, this.gridRange.maxWidth);
+    var h = Random.rangeInt(this.gridRange.minHeight, this.gridRange.maxHeight);
+    var i, j, x, y;
+    
+    this.gridWidth = w;
+    this.gridHeight = h;
+    
+    // walls for entire floor
+    for (i = 0; i < w; i += 1) {
+      this.setPos(i, 0, World.cellTypes.outerWall);
+      this.setPos(i, h - 1, World.cellTypes.outerWall);
+    }
+    for (i = 0; i < h; i += 1) {
+      this.setPos(0, i, World.cellTypes.outerWall);
+      this.setPos(w - 1, i, World.cellTypes.outerWall);
+    }
+    
+    // entire floor (without walls) as first chunk
+    var splitDir = Random.rangeInt(0, 2); // 0-1
+    var chunk = {
+      left: 1,
+      top: 1,
+      right: w - 1,
+      bottom: h - 1,
+      splitDir: splitDir
+    };
+    
+    var chunkPool = [chunk],
+      hallways = [],
+      finalChunks = [],
+      splitNum,
+      splitPos,
+      chunkA,
+      chunkB,
+      hallway,
+      upperBoundSide,
+      lowerBoundSide,
+      splitCount = 0,
+      pass = 0; // 0 = hallways and big chunks; 1 = chunks into rooms
+    
+    // partition the floor
+    
+    // start with hallways first
+    var minWidth = this.hallwayMinChunkWidth
+    var minHeight = this.hallwayMinChunkHeight;
+    var minArea = this.hallwayMinChunkArea;
+    while (chunkPool.length > 0 && splitCount < 10000) {
+      chunk = chunkPool.shift();
+      chunkA = extend({}, chunk);
+      chunkB = null;
+      splitDir = chunk.splitDir;
+      
+      if (splitDir === 0) { // split vertically
+        upperBoundSide = 'right';
+        lowerBoundSide = 'left';
+      } else { // split horizontally
+        upperBoundSide = 'bottom';
+        lowerBoundSide = 'top';
+      }
+      
+      if (pass === 0) {
+        hallway = extend({}, chunk);
+        
+        /*if (chunk.right - chunk.left > minWidth) {
+          splitNum = Random.rangeInt(2, 4);
+        } else {
+          splitNum = 2;
+        }*/
+        splitNum = 3;
+        if (splitNum === 2) { // hallway and chunk
+          if (Random.rangeInt(0, 2) === 0) {
+            splitPos = chunk[upperBoundSide] - this.hallwaySize;
+            chunkA[upperBoundSide] = splitPos - 1;
+            hallway[lowerBoundSide] = splitPos;
+          } else {
+            splitPos = chunk[lowerBoundSide] + this.hallwaySize;
+            chunkA[lowerBoundSide] = splitPos + 1;
+            hallway[upperBoundSide] = splitPos;
+          }
+        } else { // two chunks with a hallway in between
+          chunkB = extend({}, chunk);
+          splitPos = Random.rangeInt(chunk[lowerBoundSide] + minWidth / 2, chunk[upperBoundSide] - minWidth / 2);
+          chunkA[upperBoundSide] = splitPos - this.hallwaySize / 2 - 1;
+          chunkB[lowerBoundSide] = splitPos + this.hallwaySize / 2 + 1;
+          hallway[upperBoundSide] = splitPos + this.hallwaySize / 2;
+          hallway[lowerBoundSide] = splitPos - this.hallwaySize / 2;
+        }
+        hallways.push(hallway);
+      } else { // split chunks into 2
+        chunkB = extend({}, chunk);
+        splitPos = Random.rangeInt(chunk[lowerBoundSide] + minWidth / 2, chunk[upperBoundSide] - minWidth / 2);
+        chunkA[upperBoundSide] = splitPos;
+        chunkB[lowerBoundSide] = splitPos + 1;
+      }
+      
+      // if more than minChunkArea, put back into chunk pool for further partitioning
+      x = chunkA.right - chunkA.left;
+      y = chunkA.bottom - chunkA.top;
+      if (x > minWidth && y > minHeight && x * y > minArea) {
+        chunkA.splitDir = splitDir ? 0 : 1;
+        chunkPool.push(chunkA);
+      } else {
+        finalChunks.push(chunkA);
+      }
+      if (chunkB){
+        x = chunkB.right - chunkB.left;
+        y = chunkB.bottom - chunkB.top;
+        if (x > minWidth && y > minHeight && x * y > minHeight) {
+          chunkB.splitDir = splitDir ? 0 : 1;
+          chunkPool.push(chunkB);
+        } else {
+          finalChunks.push(chunkB);
+        }
+      }
+      splitCount += 1;
+      
+      if (pass === 0 && chunkPool.length === 0) {
+        pass += 1;
+        minWidth = this.minChunkWidth;
+        minHeight = this.minChunkHeight;
+        minArea = this.minChunkArea;
+        // recheck chunk sizes against new limits
+        for (i = finalChunks.length - 1; i >= 0; i -= 1) {
+          chunkA = finalChunks[i];
+          x = chunkA.right - chunkA.left;
+          y = chunkA.bottom - chunkA.top;
+          if (x > minWidth && y > minHeight && x * y > minArea) {
+            chunkPool.push(chunkA);
+            finalChunks.splice(i, 1);
+          }
+        }
+      }
+    }
+    if (splitCount >= 10000) {
+      throw new Error('Infinite loop');
+    }
+    for (i = 0; i < finalChunks.length; i += 1) {
+      chunk = finalChunks[i];
+      // put walls around final chunks
+      for (x = chunk.left - 1; x < chunk.right + 1; x += 1) {
+        this.setPos(x, chunk.top - 1, World.cellTypes.outerWall);
+        this.setPos(x, chunk.bottom, World.cellTypes.outerWall);
+      }
+      for (y = chunk.top - 1; y < chunk.bottom + 1; y += 1) {
+        this.setPos(chunk.left - 1, y, World.cellTypes.outerWall);
+        this.setPos(chunk.right, y, World.cellTypes.outerWall);
+      }
+      // set room cells as room ground
+      for (x = chunk.left; x < chunk.right; x += 1) {
+        for (y = chunk.top; y < chunk.bottom; y += 1) {
+          this.setPos(x, y, World.cellTypes.roomGround);
+        }
+      }
+      // connect rooms to hallways
+      var neighborHallways = [];
+      var rectCheck = extend({}, chunk);
+      rectCheck.left -= 2;
+      rectCheck.top -= 2;
+      rectCheck.right += 2;
+      rectCheck.bottom += 2;
+      for (j = 0; j < hallways.length; j += 1) {
+        hallway = hallways[j];
+        if (JMath.intersectRectRect(rectCheck, hallway)) {
+          neighborHallways.push(hallway);
+        }
+      }
+      if (neighborHallways.length > 0) {
+        hallway = Random.pick(neighborHallways);
+        // determine direction
+        rectCheck.top = chunk.top;
+        rectCheck.right = chunk.right;
+        rectCheck.bottom = chunk.bottom;
+        var found = false;
+        if (JMath.intersectRectRect(rectCheck, hallway)) {
+          found = true;
+          x = hallway.right;
+          y = Random.rangeInt(chunk.top, chunk.bottom);
+        }
+        if (!found) {
+          rectCheck.left = chunk.left;
+          rectCheck.top -= 2;
+          if (JMath.intersectRectRect(rectCheck, hallway)) {
+            found = true;
+            x = Random.rangeInt(chunk.left, chunk.right);
+            y = hallway.bottom;
+          }
+        }
+        if (!found) {
+          rectCheck.top = chunk.top;
+          rectCheck.right += 2;
+          if (JMath.intersectRectRect(rectCheck, hallway)) {
+            found = true;
+            x = hallway.left - 1;
+            y = Random.rangeInt(chunk.top, chunk.bottom);
+          }
+        }
+        if (!found) {
+          x = Random.rangeInt(chunk.left, chunk.right);
+          y = hallway.top - 1;
+        }
+        this.setPos(x, y, World.cellTypes.door);
+      }
     }
   },
   createCell: function (x, y, type) {
     var color = '#000000';
     switch (type) {
-    case 0: // wall
-      color = '#000000'
+    case World.cellTypes.outerWall: // wall
+      color = '#000000';
       break;
-    case 1: // floor
+    case World.cellTypes.ground: // floor
       color = '#dddddd';
+      break;
+    case World.cellTypes.door:
+      color = '#00aa00';
+      break;
+    case World.cellTypes.roomGround:
+      color = '#999999';
       break;
     }
     var item = new DisplayRect({
@@ -36,11 +261,21 @@ World.prototype = extendPrototype(DisplayContainer.prototype, {
       aabb: null
     };
   },
+  getCell: function (x, y) {
+    if (!this.grid[x] || !this.grid[x][y]) {
+      return null;
+    }
+    return this.grid[x][y];
+  },
   setPos: function (x, y, type) {
     if (!this.grid[x]) {
       this.grid[x] = {};
     }
-    var cell = this.createCell(x, y, type);
+    var cell = this.grid[x][y];
+    if (cell) {
+      this.removeChild(cell.item);
+    }
+    cell = this.createCell(x, y, type);
     this.grid[x][y] = cell;
     this.addChild(cell.item);
   }
--- src/js/src/scenes/TestScene.js
+++ src/js/src/scenes/TestScene.js
@@ -1,38 +1,52 @@
 function TestScene() {
   Scene.apply(this, arguments);
+  var bg = new DisplayRect({
+    w: SETTINGS.width,
+    h: SETTINGS.height,
+    color: '#333333'
+  });
+  this.addChild(bg);
   this.world = new World();
   this.addChild(this.world);
   this.world.generate();
+  /*var w = this.world.gridWidth * this.world.cellSize;
+  var h = this.world.gridHeight * this.world.cellSize;
+  if (w / h > SETTINGS.width / SETTINGS.height) {
+    this.world.scaleX = this.world.scaleY = SETTINGS.width / w;
+  } else {
+    this.world.scaleX = this.world.scaleY = SETTINGS.height / h;
+  }*/
+  var speed = 100;
   var vel = { x: 0, y: 0 };
   var rect = this.rect = new DisplayRect({
     w: 10,
     h: 10,
     color: 'blue'
   });
-  this.addChild(this.rect);
+  this.world.addChild(this.rect);
   this.keys = [];
   this.aKey = KB(KB.keys.a, function () {
-    vel.x = -50;
+    vel.x += -speed;
   }, function () {
-    vel.x = 0;
+    vel.x -= -speed;
   });
   this.keys.push(this.aKey);
   this.sKey = KB(KB.keys.s, function () {
-    vel.y = 50;
+    vel.y += speed;
   }, function () {
-    vel.y = 0;
+    vel.y -= speed;
   });
   this.keys.push(this.sKey);
   this.dKey = KB(KB.keys.d, function () {
-    vel.x = 50;
+    vel.x += speed;
   }, function () {
-    vel.x = 0;
+    vel.x -= speed;
   });
   this.keys.push(this.dKey);
   this.wKey = KB(KB.keys.w, function () {
-    vel.y = -50;
+    vel.y += -speed;
   }, function () {
-    vel.y = 0;
+    vel.y -= -speed;
   });
   this.keys.push(this.wKey);
   
@@ -40,6 +54,10 @@ function TestScene() {
     rect.x += vel.x * dts;
     rect.y += vel.y * dts;
   });
+  this.addSteppable(function (dts) {
+    this.world.x = SETTINGS.width / 2 - rect.x;
+    this.world.y = SETTINGS.height / 2 - rect.y;
+  }.bind(this));
 }
 TestScene.prototype = extendPrototype(Scene.prototype, {
   destroy: function () {

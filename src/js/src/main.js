cdea8c1986fdafe6641f0c9d1dd2c3ea5e9e3b0b
jayther <jayther@gmail.com>
2017-10-03 14:32:43 -0700

    Fit to screen on load and on resize.

--- src/js/src/main.js
+++ src/js/src/main.js
@@ -10,9 +10,47 @@ function Main(){
   this.animManager = new AnimManager();
   this.scene = new PlayScene(this);
   this.time = 0;
+  
+  this.resize = this.resize.bind(this);
+  window.addEventListener('resize', this.onWindowResize.bind(this), false);
+  this.onWindowResize();
+  
   this.step(0);
 }
 Main.prototype = {
+  onWindowResize: function () {
+    this.resize();
+    setTimeout(function () {
+      this.resize();
+    }.bind(this), 100);
+  },
+  resize: function () {
+    var computedStyle = window.getComputedStyle(DOM.get('root')),
+      viewWidth = parseInt(computedStyle.width, 10),
+      viewHeight = parseInt(computedStyle.height, 10),
+      canvasAspectRatio = this.canvas.width / this.canvas.height,
+      viewAspectRatio = viewWidth / viewHeight,
+      useWidth,
+      useHeight;
+    
+    if (canvasAspectRatio > viewAspectRatio) {
+      // canvas aspect ratio is wider than view's aspect ratio
+      useWidth = viewWidth;
+      useHeight = useWidth / canvasAspectRatio;
+    } else {
+      // canvas aspect ratio is taller than view's aspect ratio
+      useHeight = viewHeight;
+      useWidth = useHeight * canvasAspectRatio;
+    }
+    
+    this.canvas.style.width = Math.floor(useWidth) + 'px';
+    this.canvas.style.height = Math.floor(useHeight) + 'px';
+    this.canvas.style.position = 'absolute';
+    this.canvas.style.left = '50%';
+    this.canvas.style.top = '50%';
+    this.canvas.style.marginLeft = Math.floor(-useWidth / 2) + 'px';
+    this.canvas.style.marginTop = Math.floor(-useHeight / 2) + 'px';
+  },
   setScene: function (scene) {
     if (this.scene) {
       this.scene.destroy();
